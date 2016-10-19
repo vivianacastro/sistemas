dc3fb8ef02d1d7bff3c5ad24634506ef41e5e8ae
Juan Camilo López Espitia <juancamilolopez10a@gmail.com>
2016-10-19 08:35:07 -0500

    Configuración BD

--- modelo/config.php
+++ modelo/config.php
@@ -5,12 +5,11 @@
 class config
 {
     static public $mvc_bd_hostname = "localhost";
-    static public $mvc_bd_nombre   = "sistemas_pruebas"; //solicitudes_mantenimiento
+    static public $mvc_bd_nombre   = "sistemas"; //sistemas
     static public $mvc_bd_usuario  = "postgres";
     static public $mvc_bd_clave    = "12";
-    static public $mvc_vis_css     = "estilo.css"; 
+    static public $mvc_vis_css     = "estilo.css";
     //static public $salt = '$2a$07$G4C2/I0514B266.BBKD5ID';
 }
 
 ?>
-
