7f88b8648062739a73d9244a04d551f5b97c57dc
Juan Camilo López Espitia <juancamilolopez10a@gmail.com>
2017-09-21 15:53:15 -0500

    Paginación

--- controlador/consultas_functions.js
+++ controlador/consultas_functions.js
@@ -1,3 +1,106 @@
+/* Función que permite la paginación de las tablas */
+$.fn.pageMe = function(opts){
+    var $this = this,
+        defaults = {
+            perPage: 10,
+            showPrevNext: false,
+            hidePageNumbers: false
+        },
+        settings = $.extend(defaults, opts);
+
+    var listElement = $this.find('tbody');
+    var perPage = settings.perPage;
+    var children = listElement.children();
+    var pager = $('.pager');
+
+    if (typeof settings.childSelector!="undefined") {
+        children = listElement.find(settings.childSelector);
+    }
+
+    if (typeof settings.pagerSelector!="undefined") {
+        pager = $(settings.pagerSelector);
+    }
+
+    var numItems = children.size();
+    var numPages = Math.ceil(numItems/perPage);
+
+    pager.data("curr",0);
+
+    if (settings.showPrevNext){
+        $('<li><a href="#" class="prev_link">«</a></li>').appendTo(pager);
+    }
+
+    var curr = 0;
+    while(numPages > curr && (settings.hidePageNumbers==false)){
+        $('<li><a href="#" class="page_link">'+(curr+1)+'</a></li>').appendTo(pager);
+        curr++;
+    }
+
+    if (settings.showPrevNext){
+        $('<li><a href="#" class="next_link">»</a></li>').appendTo(pager);
+    }
+
+    pager.find('.page_link:first').addClass('active');
+    pager.find('.prev_link').hide();
+    if (numPages<=1) {
+        pager.find('.next_link').hide();
+    }
+  	pager.children().eq(1).addClass("active");
+
+    children.hide();
+    children.slice(0, perPage).show();
+
+    pager.find('li .page_link').click(function(){
+        var clickedPage = $(this).html().valueOf()-1;
+        goTo(clickedPage,perPage);
+        return false;
+    });
+    pager.find('li .prev_link').click(function(){
+        previous();
+        return false;
+    });
+    pager.find('li .next_link').click(function(){
+        next();
+        return false;
+    });
+
+    function previous(){
+        var goToPage = parseInt(pager.data("curr")) - 1;
+        goTo(goToPage);
+    }
+
+    function next(){
+        goToPage = parseInt(pager.data("curr")) + 1;
+        goTo(goToPage);
+    }
+
+    function goTo(page){
+        var startAt = page * perPage,
+            endOn = startAt + perPage;
+
+        children.css('display','none').slice(startAt, endOn).show();
+
+        if (page>=1) {
+            pager.find('.prev_link').show();
+        }
+        else {
+            pager.find('.prev_link').hide();
+        }
+
+        if (page<(numPages-1)) {
+            pager.find('.next_link').show();
+        }
+        else {
+            pager.find('.next_link').hide();
+        }
+
+        pager.data("curr",page);
+      	pager.children().removeClass("active");
+        pager.children().eq(page+1).addClass("active");
+
+    }
+};
+
 $(document).ready(function() {
     var mapaConsulta, mapaModificacion, sedeSeleccionada, campusSeleccionado, codigoSeleccionado, objetoSeleccionado, numeroFotos = 0, numeroPlanos = 0;
     var iluminacionCont = 0, cerraduraCont = 0, tomacorrientesCont = 0, puertasCont = 0, ventanasCont = 0, interruptoresCont = 0, puntosSanitariosCont = 0, lavamanosCont = 0, orinalesCont = 0, articulosCont = 0, articulosPocasUnidades = 0, proveedoresCont = 0, anadirArticulosCont = 0;
@@ -134,11 +237,13 @@ $(document).ready(function() {
             actualizarSelectMarcas("marca","electrica");
             actualizarSelectCategorias("categoria","electrica");
             actualizarSelectProveedores(proveedoresCont,"electrica");
+            $('#tabla_inventario').pageMe({pagerSelector:'#myPager',showPrevNext:true,hidePageNumbers:false,perPage:2});
         }else if (URLactual['href'].indexOf('consultar_inventario_hidraulico') >= 0 ) {
             llenarTablaInventarioHidraulico();
             actualizarSelectMarcas("marca","hidraulica");
             actualizarSelectCategorias("categoria","hidraulica");
             actualizarSelectProveedores(proveedoresCont,"hidraulica");
+            $('#tabla_inventario').pageMe({pagerSelector:'#myPager',showPrevNext:true,hidePageNumbers:false,perPage:10});
         }else if (URLactual['href'].indexOf('consultar_articulo') >= 0 ) {
             //llenarTablaInventario();
             /*actualizarSelectMarcas("marca_search");
